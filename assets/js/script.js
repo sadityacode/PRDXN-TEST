565f57991fe536dec8c8a0184a925a52718fd3e2
sadityaprdxn <adityas@prdxn.com>
2020-06-01 16:53:46 +0530

    added loader and preventback functionality js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,16 @@
 
 window.onload = function () {
 
+    var page_class = this.document.querySelector(".container");
+
+    // function for removing preloader ( THE TOP MOST COMMON FUNCTION )
+    function removeLoader() {
+      setTimeout(function () {
+        document.querySelector(".loader").classList.add("none");
+        document.querySelector("html").classList.remove("no-scroll");
+      }, 3000);
+    }
+  
+    // function for preventing the back button
+    function preventBack(){ window.history.forward(); }
 }
