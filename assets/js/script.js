9bf94a4953a38c32d4c3271469146e62ca19dd1b
sadityaprdxn <adityas@prdxn.com>
2020-06-01 18:13:39 +0530

    added form functionality and data storing functionality js

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,5 +95,153 @@ window.onload = function () {
       document.querySelector(".modal").classList.remove("block");
     })
 
+    // function for sign up form validation
+    signupFieldsArray.forEach(function (element) {
+      var inputRegex = element.getAttribute("data-regex");
+      for (var key in RegexExpression) {
+
+        if (inputRegex === key) {
+
+          var regexForThis = RegexExpression[key];
+
+          element.addEventListener("keyup", function () {
+            validate(regexForThis, this);
+          })
+        }
+      }
+    });
+
+    // function for form validate the regex
+    function validate(RegularExpression, input) {
+      var parent = input.parentNode;
+      if (input.value == "") {
+        parent.classList = "form-group";
+      }
+      else if (RegularExpression.test(input.value)) {
+        parent.classList = "form-group success"
+      } else {
+        parent.classList = "form-group error"
+      }
+    }
+
+    // function for sign in form validation
+    signinFieldsArray.forEach(function (element) {
+      element.addEventListener("keyup", function () {
+        element.parentElement.classList = "form-group";
+      })
+    });
+
+
+    // function for form submission
+    submitForm.forEach(function (element) {
+
+      if (element.classList.contains("sign-up")) {
+        element.addEventListener("click", storeData);
+      }
+
+      if (element.classList.contains("sign-in")) {
+        element.addEventListener("click", verifyUser);
+      }
+    });
+
+    // function for storing data
+    function storeData(e) {
+      e.preventDefault();
+
+      var allFeildsRight = null;
+      var newEmail = document.querySelector(".sign-up-email");
+
+      // checking the all feilds are right or wrong
+      for (var i = 0; i < signupFieldsArray.length; i++) {
+        var parent = signupFieldsArray[i].parentNode;
+        if (parent.classList.contains("error") || parent.classList == "form-group") {
+          allFeildsRight = false;
+          signupFieldsArray.forEach(function (element) {
+            if (element.value === "") {
+              element.parentNode.classList.add("error");
+            }
+          });
+          break;
+        } else if (parent.classList.contains("success")) { allFeildsRight = true; }
+      }
+
+      // checking the email-id already registered or not
+      var UserAlreadyExist = checkUser(newEmail);
+
+      // function if both the condition are satisfying
+      if (allFeildsRight && !UserAlreadyExist) {
+
+        var username = document.querySelector(".sign-up-name");
+        var email = document.querySelector(".sign-up-email");
+        var password = document.querySelector(".sign-up-password");
+        alert("FORM SUCCESSFULLY SUBMITED");
+
+        var unicode = "tmdbuser" + window.localStorage.length;
+
+        var data = {
+          name: username.value,
+          mail: email.value,
+          pass: password.value,
+          unicode: unicode
+        }
+
+        var data_serialized = JSON.stringify(data);
+
+        window.localStorage.setItem(unicode, data_serialized);
+
+        formReset();
+        window.location.assign("movies.html?userid=" + unicode + "&category=movie");
+      } else { newEmail.parentElement.classList = "form-group error" }
+    }
+
+    // function for checking the user-is exist or not
+    function checkUser(Email) {
+      if (localStorage.length !== 0) {
+        for (var key in localStorage) {
+          if (key === "length") { break; }
+          else if (key.includes("tmdbuser")) {
+            var parseData = JSON.parse(localStorage[key]);
+
+            if (Email.value === parseData.mail) {
+              return parseData;
+            }
+          }
+        } return false;
+      } else { return false; }
+    }
+
+    // function for verifying the user
+    function verifyUser() {
+      var signInEmail = document.querySelector(".sign-in-email");
+
+      var UserAlreadyExist = checkUser(signInEmail);
+      if (UserAlreadyExist) {
+        var signInPassword = document.querySelector(".sign-in-password");
+        var password = UserAlreadyExist.pass;
+        if (signInPassword.value === password) {
+          formReset();
+          var unicode = UserAlreadyExist.unicode;
+          window.location.assign("movies.html?userid=" + unicode + "&category=movie");
+        } else { signInPassword.parentElement.classList.add("error") }
+      } else { signInEmail.parentElement.classList.add("error") }
+    }
+
+    // function for resetting the form
+    function formReset() {
+      var forms = Array.from(document.querySelectorAll("form"));
+
+      forms.forEach(function (element) {
+        element.reset();
+
+        var formGroups = Array.from(element.querySelectorAll(".form-group"));
+        formGroups.forEach(function (element) {
+          element.classList = "form-group";
+        });
+      });
+    }
+
+    // for removing preloader
+    removeLoader();
+
   }
 }
